4c40a2ed5a641d43a706757ca8da5e5e170df2b9
Davanon <davanon2014@gmail.com>
2017-08-21 16:18:59 +0530

    First

---
+++ src/main/java/com/iqmsoft/payara/jpa/MyEntity.java
@@ -0,0 +1,53 @@
+
+package com.iqmsoft.payara.jpa;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+public class MyEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+    
+        if (!(object instanceof MyEntity)) {
+            return false;
+        }
+        MyEntity other = (MyEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "test[ id=" + id + " ]";
+    }
+    
+}
